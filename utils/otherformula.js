d922cbd2810e38d1ebb8991aac6f9e5ad8917322
Unsignia <csdedwards@gmail.com>
2023-02-19 14:43:44 -0500

    fix 2

--- utils/otherformula.js
+++ utils/otherformula.js
@@ -1,5 +1,8 @@
 export function salary(missing, sal, days, hours, hourly) {
   if (missing == "sal") {
-    return (52 * days * hours * hourly).toFixed(2).toLocaleString("en-US");
+    return (52 * days * hours * hourly).toLocaleString("en-US", {
+      minimumFractionDigits: "2",
+      maximumFractionDigits: "2",
+    });
   }
 }
